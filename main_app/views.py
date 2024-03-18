b9514c6e5b8a5cf3717cd40c11e0ffe2337a1a50
treyweldon <trey@millcreeklimited.com>
2024-03-18 11:49:42 -0400

    first commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def categories(request):
+  return render(request, 'categories/index.html')
+
+def listings(request):
+  return render(request, 'categories/listings.html')
+
+def register(request):
+  return render(request, 'register/index.html')
+
+def reg_client(request):
+  return render(request, 'register/client.html')
+
+def reg_freelancer(request):
+  return render(request, 'register/freelancer.html')
+
+def profile_client(request):
+  # client = Client.objects.get(id=client_id)
+  return render(request, 'profile/client.html', {})
+
+def profile_freelancer(request):
+  # freelancer = Freelancer.objects.get(id=freelancer_id)
+  return render(request, 'profile/freelancer.html', {})
+
+# class JobCreate(CreateView):
+#   model = JobPosting
+#   fields = '__all__'
+
+# class JobUpdate(UpdateView):
+#   model = JobPosting
+#   fields = []
+
+# class JobDelete(DeleteView):
+#   model = JobPosting
+
+# class JobList(ListView):
+#   model = JobPosting
+
+# class JobDetail(DetailView):
+#   model = JobPosting
