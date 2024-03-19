9a3f72b2219232673edafbe537187c7f449fc85f
dalia <awawdeh.dalia@gmail.com>
2024-03-19 08:28:36 -0500

    UI update for the homePage

--- Freelancer/settings.py
+++ Freelancer/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'  # Redirect to the home page after logout
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
