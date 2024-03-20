85acf3ea6ff058d30079b0a4d3db01c0266a8165
GitGoodDan <d7ibton@gmail.com>
2024-03-20 10:38:00 -0400

    added search function to be category specific

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import JobPosting, UserProfile, ClientProfile, FreelancerProfile
+from .models import JobPosting, ClientProfile, FreelancerProfile
 
 # Register your models here.
-admin.site.register(UserProfile)
 admin.site.register(ClientProfile)
 admin.site.register(FreelancerProfile)
 admin.site.register(JobPosting)
